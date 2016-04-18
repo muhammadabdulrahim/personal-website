db294c7b030136a0bc213abc9323b92c18a70157
Muhammad Abdul-Rahim <moobeidallah@gmail.com>
2016-04-18 18:44:44 -0400

    Framework

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php include('parts/header.php'); ?>
 
 <h1>Muhammad Abdul-Rahim</h1>
+<h2>Web, Mobile, Game Developer</h2>
+<h3>Greater Washington DC Area</h3>
 
 <?php include('parts/footer.php'); ?>
