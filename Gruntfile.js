452844b8a4e2ae080f76d0b5cb0bf4c53dd51e82
Muhammad Abdul-Rahim <moobeidallah@gmail.com>
2016-04-18 18:51:14 -0400

    Gruntfile placed

---
+++ Gruntfile.js
@@ -0,0 +1,11 @@
+module.exports = function(grunt){
+	
+	//	Project config
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+	});
+	
+
+	//	Config tasks
+	grunt.registerTask('default',[]);
+};
