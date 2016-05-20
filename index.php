a8808fa3a44eef93c6af606479b613a2ef5e7538
Muhammad Abdul-Rahim <moobeidallah@gmail.com>
2016-05-20 00:31:07 -0400

    Added photography panel and some content

--- index.php
+++ index.php
@@ -1,6 +1,42 @@
 <?php include('/parts/header.php'); ?>
 
-<div id="container"></div>
+<div id="container">
+	<h1>Hello! I'm Muhammad Abdul-Rahim!</h1>
+	<h2>GWU 2014 Magna Cum Laude &mdash; B.S. Computer Engineering, Minor Computer Science</h2>
+	<p>&emsp;I'm a full-time software developer with a hardware background and a penchant for minimal desgin and development. I have a mobile-first mentality and avoid paradigms that do not work well with modern mobile design. I love JavaScript&mdash;seriously, it's a truly fascinating language!&mdash;but I avoid it when I can; after all, it can just be disabled. I've developed for many platforms using many languages. Here's a small sample:</p>
+	<ul>
+		<li>Linux &mdash; Used C to extend file system attributes in Linux 2.6.18</li>
+		<li>FPGA &mdash; Wrote hardware-level Verilog to recreate <em>Duck Hunt</em> on a Basys 2 Spartan-3E board</li>
+		<li>Arduino &mdash; Used C to write an on-board controller for an autonomous search-and-rescue robot</li>
+		<li>PC, OS X, Linux &mdash; Managed multiple game projects written in C# and made with Unity, from start to finish</li>
+		<li>Android, iOS &mdash; Created cross-platform native applications with shared subrepositories</li>
+		<li>WordPress &mdash; Developed multiple sites for a variety of clients with PHP, CSS, JS, and the like</li>
+	</ul>
+	<p>Want to learn more about what I've done? Scroll down and discover more!</p>
+</div>
+
+<!--TODO: Move these to their own partials -->
+<div id="photography">
+	<div class="inner">
+		<h1>My Photography</h1>
+		<p>I sometimes take photographs! Here are a few of my favorites:</p>
+		<!--TODO: Click to modal -->
+		<div class="image-portfolio">
+			<div class="image">
+				<img src="<?php echo get_assets_uri(); ?>/images/IMG_5522.JPG">
+				<span>Bird &ndash; Maryland</span>
+			</div>
+			<div class="image">
+				<img src="<?php echo get_assets_uri(); ?>/images/IMG_6262.JPG">
+				<span>Bear &ndash; Ontario</span>
+			</div>
+			<div class="image">
+				<img src="<?php echo get_assets_uri(); ?>/images/IMG_6044.JPG">
+				<span>Deer &ndash; Ontario</span>
+			</div>
+		</div>
+	</div>
+</div>
 
 
 <?php include('/parts/footer.php'); ?>
--- parts/aside.php
+++ parts/aside.php
@@ -1,13 +1,15 @@
 <div id="aside">
-	<img src="<?php echo get_assets_uri(); ?>/images/muhammad.jpg">
+	<img alt="Picture of Muhammad Abdul-Rahim" src="<?php echo get_assets_uri(); ?>/images/muhammad.jpg">
 	<h1><span>Muhammad</span><span>Abdul-Rahim</span></h1>
 	<h2>Washington D.C. Area</h2>
 	<h3>Web, Mobile, Game Developer</h3>
 	
-	<ul>
+	<ul class="contact-panel">
 		<!-- TODO: Open a modal to avoid spam from directing -->
-		<li class="email"><a href="#">email</a></li>
-		<li class="github"><a href="https://github.com/muhammadabdulrahim" target="_blank">github</a></li>
+		<li><a href="#">email</a></li>
+		<li><a href="https://github.com/muhammadabdulrahim" target="_blank">github</a></li>
 	</ul>
+	
+	<p>C, C#, PHP, JS, CSS/LESS/SASS</p>
 	
 </div>
