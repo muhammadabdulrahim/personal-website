f089f55ad924eadb950a5541fa1f33526e31a940
Muhammad Abdul-Rahim <moobeidallah@gmail.com>
2016-08-05 17:40:04 -0400

    Update aside

--- parts/aside.php
+++ parts/aside.php
@@ -1,15 +1,13 @@
 <div id="aside">
 	<img alt="Picture of Muhammad Abdul-Rahim" src="<?php echo get_assets_uri(); ?>/images/muhammad.jpg">
 	<h1><span>Muhammad</span><span>Abdul-Rahim</span></h1>
-	<h2>Washington D.C. Area</h2>
+	<h2>Washington, D.C. Area</h2>
 	<h3>Web, Mobile, Game Developer</h3>
 	
 	<ul class="contact-panel">
-		<!-- TODO: Open a modal to avoid spam from directing -->
-		<li><a href="#">email</a></li>
-		<li><a href="https://github.com/muhammadabdulrahim" target="_blank">github</a></li>
+		<li>moobeidallah [at] gmail [dot] com</li>
 	</ul>
 	
-	<p>C, C#, PHP, JS, CSS/LESS/SASS</p>
+	<p>C, C#, Java, PHP, JS</p>
 	
 </div>
