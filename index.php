1d3420ac0d567f04877d3f8d44ebe4db15892fb1
Muhammad Abdul-Rahim <moobeidallah@gmail.com>
2016-08-05 18:04:25 -0400

    Games partial, incomplete, added, some touchup also

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php include('parts/header.php'); ?>
 <?php include('parts/aside.php'); ?>
 <?php include('parts/container.php'); ?>
+<?php include('parts/games.php'); ?>
 <?php include('parts/photography.php'); ?>
 <?php include('parts/footer.php'); ?>
--- parts/container.php
+++ parts/container.php
@@ -1,7 +1,7 @@
 <div id="container">
 	<h1>Hello! I'm Muhammad Abdul-Rahim!</h1>
 	<h2>GWU 2014 Magna Cum Laude &mdash; B.S. Computer Engineering, Minor Computer Science</h2>
-	<p>&emsp;I'm a full-time software developer with a hardware background and a penchant for minimal desgin and development. I have a mobile-first mentality and avoid paradigms that do not work well with modern mobile design. I love JavaScript&mdash;seriously, it's a truly fascinating language!&mdash;but I avoid it when I can; after all, it can just be disabled. I've developed for many platforms using many languages. Here's a small sample:</p>
+	<p>I'm a full-time software developer with a hardware background and a penchant for minimal desgin and development. I have a mobile-first mentality and avoid paradigms that do not work well with modern mobile design. I love JavaScript&mdash;seriously, it's a truly fascinating language!&mdash;but I avoid it when I can; after all, it can just be disabled. I've developed for many platforms using many languages. Here's a small sample:</p>
 	<ul>
 		<li>Linux &mdash; Used C to extend file system attributes in Linux 2.6.18</li>
 		<li>FPGA &mdash; Wrote hardware-level Verilog to recreate <em>Duck Hunt</em> on a Basys 2 Spartan-3E board</li>
