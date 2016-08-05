1c729c20e4e773727d7f9a5af87bd14bf6ea1fe2
Muhammad Abdul-Rahim <moobeidallah@gmail.com>
2016-08-05 17:41:26 -0400

    Remove list from the aside, use a paragraph tag

--- parts/aside.php
+++ parts/aside.php
@@ -3,11 +3,7 @@
 	<h1><span>Muhammad</span><span>Abdul-Rahim</span></h1>
 	<h2>Washington, D.C. Area</h2>
 	<h3>Web, Mobile, Game Developer</h3>
-	
-	<ul class="contact-panel">
-		<li>moobeidallah [at] gmail [dot] com</li>
-	</ul>
-	
+	<p>moobeidallah [at] gmail [dot] com</p>
 	<p>C, C#, Java, PHP, JS</p>
 	
 </div>
