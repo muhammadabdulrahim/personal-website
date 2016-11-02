55f16379a1661c4ee11e8cc562ae652b4c6597dd
Muhammad Abdul-Rahim <moobeidallah@gmail.com>
2016-11-02 03:23:34 -0400

    Make printing of reusable nodes fit MVC paradigm better

--- parts/functions.php
+++ parts/functions.php
@@ -7,7 +7,7 @@ function get_home_uri()
 	$https = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https://' : 'http://';
 	$server = isset($_SERVER['SERVER_NAME']) ? $_SERVER['SERVER_NAME'] : '';
 	$request = isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : '';
-	
+
 	return $https.$server.$request;
 }
 
@@ -21,30 +21,7 @@ function pre_print_r($s)
 	?><pre><?php print_r($s); ?></pre><?php
 }
 
-function create_portfolio($pkg)
+function iskeyset($key,$arr)
 {
-	if( empty($pkg) ) return;
-	
-	//	TODO: Add links offsite and also to modals
-	?>
-	<div class="portfolio">
-		<?php foreach( $pkg as $node ): ?>
-		<div class="node">
-			
-			<?php if( array_key_exists('img',$node) ): ?>
-				<img src="<?php echo $node['img']; ?>">
-			<?php endif; ?>
-			
-			<?php if( array_key_exists('title',$node) ): ?>
-				<h1><?php echo $node['title']; ?></h1>
-			<?php endif; ?>
-			
-			<?php if( array_key_exists('caption',$node) ): ?>
-				<span><?php echo $node['caption']; ?></span>
-			<?php endif; ?>
-			
-		</div>
-		<?php endforeach; ?>
-	</div>
-	<?php
+	return array_key_exists($key,$arr) && !empty($arr[$key]);
 }
--- parts/view.php
+++ parts/view.php
@@ -3,6 +3,31 @@
 	<div class="inner">
 		<h2><?php echo $data['title']; ?></h2>
 		<p><?php echo $data['content']; ?></p>
-		<?php if( array_key_exists('pkg',$data) ) create_portfolio($data['pkg']); ?>
+		<?php
+			if( iskeyset('pkg',$data) )
+			{
+				?><div class="portfolio"><?php
+				foreach( $data['pkg'] as $node)
+				{
+					?><div class="node"><?php
+					if( iskeyset('img',$node) )
+					{
+						?><img src="<?php echo $node['img']; ?>"><?php
+					}
+
+					if( iskeyset('title',$node) )
+					{
+						?><span class="title"><?php echo $node['title']; ?></span><?php
+					}
+
+					if( iskeyset('caption',$node) )
+					{
+						?><span class="caption"><?php echo $node['caption']; ?></span><?php
+					}
+					?></div><?php
+				}
+				?></div><?php
+			}
+		?>
 	</div>
 </div>
