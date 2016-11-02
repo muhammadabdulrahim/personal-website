ab4f31aae226039cbfc9a643247f26318cb893d6
Muhammad Abdul-Rahim <moobeidallah@gmail.com>
2016-11-02 02:48:34 -0400

    Adjust styles further, add Projects image

--- parts/aside.php
+++ parts/aside.php
@@ -1,9 +1,8 @@
 <div id="aside">
 	<img alt="Picture of Muhammad Abdul-Rahim" src="<?php echo get_assets_uri(); ?>/images/muhammad.jpg">
-	<h1><span>Muhammad</span><span>Abdul-Rahim</span></h1>
-	<h2>Washington, D.C. Area</h2>
-	<h3>Web, Mobile, Game Developer</h3>
-	<p>moobeidallah [at] gmail</p>
+	<p class="title"><span>Muhammad</span><span>Abdul-Rahim</span></p>
+	<p class="byline">Washington, D.C. Area</p>
+	<p class="em">Web, Mobile, Game Developer</p>
+	<p><a href="mailto:moobeidallah@gmail.com">email</a>&emsp;<a href="https://marimodev.itch.io/">itchio</a>&emsp;<a href="https://twitter.com/marimodev">twitter</a></p>
 	<p>C, C#, Java, PHP, JS</p>
-	
 </div>
